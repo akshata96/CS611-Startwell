f209552a42059b0cc5e0d44d98881958f47e3692
akshata96 <36476401+akshata96@users.noreply.github.com>
2021-04-09 08:52:44 -0600

    Update SignUp.js

--- startwellfrontend/src/Components/SignUp/SignUp.js
+++ startwellfrontend/src/Components/SignUp/SignUp.js
@@ -189,7 +189,7 @@ class SignUp extends Component {
     if (isValid) {
       console.log("Posting");
       axios
-        .post("http://localhost:3200/user/signup", {
+        .post("http://206.189.195.166:3200/user/signup", {
           user: {
             firstname: firstname,
             lastname: lastname,

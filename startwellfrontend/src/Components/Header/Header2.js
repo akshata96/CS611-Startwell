9038248a5de20103085cdcdf85b376608d4c305a
akshata96 <akshata.akki96@gmail.com>
2021-04-30 00:50:15 -0400

    minor changes

--- startwellfrontend/src/Components/Header/Header2.js
+++ startwellfrontend/src/Components/Header/Header2.js
@@ -45,24 +45,24 @@ export default class Header2 extends Component {
     
     return (
       <div>
-         <Header style={{ backgroundColor: 'gray', height: '100%' }}>
+        <Header style={{ backgroundColor: 'gray', height: '100%' }}>
           <Menu mode='horizontal' style={{ width: '100%', height: '100%', backgroundColor: 'gray' }}>
-         <a href = {'/Homepage?token=' + String(this.state.token)}> <img src={logo} width={180} /> </a>
-            
-           <Menu.Item key='Sign Up/Log In'  className='Topnav' style={{ color: 'white' }} onClick={this.handleSuccessfulAuth}>
-                {this.state.fname}
+         <a href = '/Homepage'> <img src={logo} width={180} /> </a>
+            <Menu.Item key='Sign Up/Log In' className='Topnav'>
+              <a href='/Login' style={{ color: 'white' }}>
+                Log In
+              </a>
             </Menu.Item>
             <Menu.Item key='About' className='Topnav'>
-              <a href={'/About?token=' + String(this.state.token)} style={{ color: 'white' }}>
+              <a href='/About' style={{ color: 'white' }}>
                 About
               </a>
             </Menu.Item>
             <Menu.Item key='Home' className='Topnav'>
-              <a href={'/Homepage?token=' + String(this.state.token)} style={{ color: 'white' }}>
+              <a href='/Homepage' style={{ color: 'white' }}>
                 Home
               </a>
             </Menu.Item>
-            
           </Menu>
         </Header>
       </div>
